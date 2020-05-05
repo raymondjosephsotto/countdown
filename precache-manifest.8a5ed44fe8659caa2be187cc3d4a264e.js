9e8da0d2a5296076ef05214ef6d7afd78fee3d98
Raymond Sotto <raymondjosephsotto@gmail.com>
2020-05-05 16:40:37 -0400

    Updates

---
+++ precache-manifest.8a5ed44fe8659caa2be187cc3d4a264e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656f678dda62f99c656c80febd5b14c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6d9d5ff2412bbc387de",
+    "url": "/static/css/main.04970afc.chunk.css"
+  },
+  {
+    "revision": "a37e9c83531e65621827",
+    "url": "/static/js/2.fadb6353.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fadb6353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d9d5ff2412bbc387de",
+    "url": "/static/js/main.64dbddf1.chunk.js"
+  },
+  {
+    "revision": "6ee2ba46a34cd00b0438",
+    "url": "/static/js/runtime-main.f2eb7a5a.js"
+  },
+  {
+    "revision": "186deae5a046c0fcd9b4654a8b995151",
+    "url": "/static/media/black-hand.186deae5.jpg"
+  },
+  {
+    "revision": "9e4c42a013e08d244378c3182fc7f12a",
+    "url": "/static/media/sinag-logo.9e4c42a0.png"
+  }
+]);
